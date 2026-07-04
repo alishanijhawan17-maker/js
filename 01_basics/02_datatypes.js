e37aed78ce90f7db0ca241c262e0cc271c3e5baa
alishanijhawan17-maker <alishanijhawan17@gmail.com>
2026-07-04 13:33:13 +0000

    success

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all JS code as newer version
+//alert(3+3)//we are using nodejs not browser
+let name="alisha"//string datatype
+let age=12//int data type
+let isLoggedIn=false//boolean data type
+//number ==>2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null==>standalone value[means khud intentionally humne null value assign krdi]
+//undefined==>means value assigned nhi hai
+//symbol==> unique 
+console.log(typeof age)//type-->number
+console.log(typeof null)//type-->object
+console.log(typeof undefined)//type-->undefined.  
+
+
+
+//object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,62 @@
+// let score="23"
+// console.log(typeof(score))//string
+// let valueInNumber=Number(score)//string-->number
+// console.log(typeof(valueInNumber))
+
+// let a="33abc"
+// console.log(typeof(a));//string
+// let b=Number(a)
+// console.log(typeof(b));//number
+// console.log(a);
+// console.log(b);//NaN
+//"33"--> 33
+//"33abc"-->NaN
+//true-->1 /false-->0
+//conversion to boolean:
+//1-->true ; 0-->false
+//""-->false i.e empty string to boolean gives false
+//"abc"-->true i.e any string inside it gives true
+
+
+//********OPERATIONS */
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);//0.666666 
+console.log(2%3);
+let str1="hello"
+let str2="world"
+let str3=str1+str2
+console.log(str3);//helloworld
+//str1="hello"
+//str2=" world"
+//str3=hello world
+console.log(1+"2");//12
+console.log("1"+2);//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
